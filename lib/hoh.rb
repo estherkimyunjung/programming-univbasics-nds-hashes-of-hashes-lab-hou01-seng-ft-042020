e191b18cccc38ea2d6165af5788d0a3b648cbdd0
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-22 23:51:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -56,9 +56,9 @@ def naming_system
           sub_category:
           {
             label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
+  sub_category: {
+    label: "Genus",
+  }
           }
         }
       }
